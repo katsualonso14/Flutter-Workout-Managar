1e867dbe417f42cf523cfb04bef3f4328afba391
KatsuyaTamai <katsuyatamai@MacBook-Pro.local>
2023-02-23 09:41:43 +0900

    add button for add page Screen transition

---
+++ lib/presentation/page/add_page.dart
@@ -0,0 +1,16 @@
+//記録追加ページ
+
+import 'package:flutter/material.dart';
+
+class AddPage extends StatelessWidget {
+  const AddPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+          child: Text('AddPage'),
+        ),
+    );
+  }
+}
---
+++ lib/presentation/page/level_manage_page.dart
@@ -0,0 +1,16 @@
+// 筋トレレベル管理ページ
+
+import 'package:flutter/material.dart';
+
+class LevelManagePage extends StatelessWidget {
+  const LevelManagePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+          child: Text('LevelManage'),
+        ),
+      );
+  }
+}
