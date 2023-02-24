f67e11edcab2c38c16378d8bd6af94b09d83a3ff
KatsuyaTamai <katsuyatamai@MacBook-Pro.local>
2023-02-24 20:38:40 +0900

    add Caleandar Event dots

--- lib/presentation/page/calender_page.dart
+++ lib/presentation/page/calender_page.dart
@@ -1,4 +1,6 @@
 //カレンダーページ
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
@@ -10,9 +12,15 @@ class CalenderPage extends StatefulWidget {
 }
 
 class _CalenderPageState extends State<CalenderPage> {
-  DateTime _focusedDay = DateTime.now(); //現在日
+  DateTime _focusedDay = DateTime.now(); // 現在日
   CalendarFormat _calendarFormat = CalendarFormat.month; // 月フォーマット
-  DateTime? _selectedDay; //選択している日付
+  DateTime? _selectedDay; // 選択している日付
+
+  //Map形式で保持　keyが日付　値が文字列
+  final sampleMap = {
+    DateTime.utc(2023, 2,20): ['firstEvent', 'secondEvent'],
+    DateTime.utc(2023, 2,5): ['thirdEvent', 'fourthEvent'],
+  };
 
   @override
   Widget build(BuildContext context) {
@@ -22,8 +30,11 @@ class _CalenderPageState extends State<CalenderPage> {
           firstDay: DateTime.utc(2023, 1, 1),
           lastDay: DateTime.utc(2024, 12, 31),
           focusedDay: _focusedDay,
-          calendarFormat: _calendarFormat, //デフォを月表示に設定
-          //「月」「週」変更
+          eventLoader: (date){
+            return sampleMap[date] ?? [];
+          },
+          calendarFormat: _calendarFormat, // デフォを月表示に設定
+          // 「月」「週」変更
           onFormatChanged: (format) {
             if (_calendarFormat != format) {
               setState(() {
@@ -31,6 +42,7 @@ class _CalenderPageState extends State<CalenderPage> {
               });
             }
           },
+          // 選択日のアニメーション
           selectedDayPredicate: (day) {
             return isSameDay(_selectedDay, day);
           },
