e3d18778c1f298d620db4d342d80d5f3a9d688dd
katsuyatamai <xabi.k0124football@gmail.com>
2024-09-25 17:06:37 +0200

    add faunction that delete acount

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_workout_manager/presentation/pages/calender_page.dart';
@@ -9,7 +10,8 @@ import 'core/firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const ProviderScope(child: App()));
+  runApp(const ProviderScope(
+      child: MaterialApp(debugShowCheckedModeBanner: false, home: App())));
 }
 
 class App extends HookConsumerWidget {
@@ -18,10 +20,137 @@ class App extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final userCheck = ref.watch(userCheckProvider);
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Flutter Workout Manager'),),
+
+    void _performSensitiveAction(BuildContext context) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Success'),
+            content: const Text('パスワードの確認が完了し、アカウントを削除いたしました。'),
+            actions: [
+              TextButton(
+                onPressed: () async {
+                  await FirebaseAuth.instance.currentUser!.delete();
+                  Navigator.pop(context);
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+
+
+
+    void _reauthenticateUser(BuildContext context, String password) async {
+      User? user = FirebaseAuth.instance.currentUser;
+
+      if (user != null) {
+        try {
+          String email = user.email!;
+          final credential = EmailAuthProvider.credential(email: email, password: password);
+          await user.reauthenticateWithCredential(credential);
+
+          _performSensitiveAction(context);
+
+        } catch (e) {
+          print('Re-authentication failed: $e');
+        }
+      }
+    }
+
+    Future<void> _showPasswordDialog(BuildContext parentContext) async {
+      final TextEditingController passwordController = TextEditingController();
+      print('showPasswordDialog');
+
+      await showDialog(
+        context: parentContext,  // Pass the context from the parent widget
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('削除するにはパスワードを入力してください'),
+            content: TextField(
+              controller: passwordController,
+              decoration: const InputDecoration(labelText: 'Enter your password'),
+              obscureText: true,
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);  // Close the dialog
+                },
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () {
+                  final password = passwordController.text;
+                  if (password.isNotEmpty) {
+                    Navigator.pop(context);  // Close the dialog
+                     _reauthenticateUser(parentContext, password);  // Pass the parent context
+                  } else {
+                    ScaffoldMessenger.of(parentContext).showSnackBar(
+                      const SnackBar(content: Text('Please enter a password')),
+                    );
+                  }
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+
+
+    Future<void> deleteUserAccount() async {
+      try {
+        await FirebaseAuth.instance.currentUser!.delete();
+      } on FirebaseAuthException catch (e) {
+        print('error: $e');
+        await _showPasswordDialog(context);
+      } catch (e) {
+        print(e);
+      }
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Workout Manager'),
+          actions: [
+            // アカウント削除
+            IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: const Text('アカウント削除'),
+                        content: const Text('はいをタップするとアカウントが削除されます。\n本当に削除しますか？'),
+                        actions: [
+                          TextButton(
+                            onPressed: () async {
+                              await deleteUserAccount();
+                              Navigator.pop(context);
+                            },
+                            child: const Text('はい'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text('いいえ'),
+                          ),
+                        ],
+                      );
+                    }
+                );
+              },
+            ),
+          ],
+        ),
+
         body: userCheck.when(
             error: (error, stackTrace) {
               return const Center(child: Text('エラーが発生しました'));
@@ -37,7 +166,6 @@ class App extends HookConsumerWidget {
               }
             }
         ),
-      ),
-    );
+      );
   }
 }
