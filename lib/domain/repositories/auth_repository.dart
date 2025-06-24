1e46da34602ed5b975c2fc176beb527ce6cc7ecc
katsuyatamai <xabi.k0124football@gmail.com>
2025-06-24 18:18:47 +0900

    不要な定義を削除

--- lib/domain/repositories/auth_repository.dart
+++ lib/domain/repositories/auth_repository.dart
@@ -1,10 +1,9 @@
-import 'package:flutter_workout_manager/domain/entities/auth_result_entity.dart';
 import 'package:flutter_workout_manager/domain/entities/user_entity.dart';
 
 // FirebaseAuthのData層とPresentation層の間のインターフェース
 abstract class AuthRepository {
   Stream<UserEntity?> authStateChanges();
-  Future<AuthResultEntity> signIn({required String email, required String password});
+  Future<UserEntity> signIn({required String email, required String password});
   Future<UserEntity> register({required String email, required String password});
   Future<void> deleteUser();
 }
