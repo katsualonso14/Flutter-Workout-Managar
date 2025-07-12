fb7694823626b75f8fbd0919f066d85d6e7207af
katsuyatamai <xabi.k0124football@gmail.com>
2025-07-12 10:04:41 +0900

    Rename text_filed directory

--- lib/presentation/pages/login.dart
+++ lib/presentation/pages/login.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_workout_manager/presentation/controller/login_form_providers.dart';
 import 'package:flutter_workout_manager/presentation/controller/sign_in_user_notifier.dart';
 import 'package:flutter_workout_manager/presentation/controller/user_register_provider.dart';
 import 'package:flutter_workout_manager/presentation/pages/no_login_calender_page.dart';
-import 'package:flutter_workout_manager/presentation/controller/login_form_providers.dart';
 import 'package:flutter_workout_manager/presentation/widgets/medium_ad_banner.dart';
-import 'package:flutter_workout_manager/presentation/widgets/test_field/mail_address_text_form_field.dart';
-import 'package:flutter_workout_manager/presentation/widgets/test_field/password_text_form_filed.dart';
+import 'package:flutter_workout_manager/presentation/widgets/text_field/mail_address_text_form_field.dart';
+import 'package:flutter_workout_manager/presentation/widgets/text_field/password_text_form_filed.dart';
 
 class LogIn extends ConsumerWidget {
   const LogIn({Key? key}) : super(key: key);
@@ -28,46 +28,52 @@ class LogIn extends ConsumerWidget {
               children: <Widget>[
                 const MailAddressTextFormField(),
                 const PasswordTextFormFiled(),
-                const SizedBox(height: 30,),
+                const SizedBox(
+                  height: 30,
+                ),
                 SizedBox(
                   width: double.infinity,
                   child: ElevatedButton(
-                      onPressed: () async {
-                        try {
-                          // ユーザー登録
-                             await ref.read(userRegisterProvider.notifier)
-                                  .registerUser(userEmail.state, userPassword.state);
-                          if( context.mounted) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(content: Text('User registered successfully: ${userEmail.state}')),
-                            );
-                          }
-                        } catch (e) {
-                          // 登録に失敗した場合
-                          if (context.mounted) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                  content: Text(
-                                      'failed to register. Please try again.'
-                                      '\nPlease enter a password of 6 characters or more.'
-                                      '\nPlease enter a valid email address')),
-                            );
-                          }
+                    onPressed: () async {
+                      try {
+                        // ユーザー登録
+                        await ref
+                            .read(userRegisterProvider.notifier)
+                            .registerUser(userEmail.state, userPassword.state);
+                        if (context.mounted) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                                content: Text(
+                                    'User registered successfully: ${userEmail.state}')),
+                          );
                         }
-                      },
+                      } catch (e) {
+                        // 登録に失敗した場合
+                        if (context.mounted) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text(
+                                    'failed to register. Please try again.'
+                                    '\nPlease enter a password of 6 characters or more.'
+                                    '\nPlease enter a valid email address')),
+                          );
+                        }
+                      }
+                    },
                     style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue, // ボタンの背景色
-                        foregroundColor: Colors.white, // ボタンの文字色
+                      backgroundColor: Colors.blue, // ボタンの背景色
+                      foregroundColor: Colors.white, // ボタンの文字色
                     ),
-                      child: const Text('Register User Account'),
+                    child: const Text('Register User Account'),
                   ),
-
                 ),
                 SizedBox(
                     width: double.infinity,
                     child: ElevatedButton(
                       // 無効化（多重タップ防止）
-                      onPressed: signInState.isLoading ? null : () async {
+                      onPressed: signInState.isLoading
+                          ? null
+                          : () async {
                               try {
                                 await ref
                                     .read(signInUserNotifierProvider.notifier)
@@ -75,7 +81,8 @@ class LogIn extends ConsumerWidget {
                                         userEmail.state, userPassword.state);
                                 if (context.mounted) {
                                   ScaffoldMessenger.of(context).showSnackBar(
-                                    const SnackBar(content: Text('Login successful')),
+                                    const SnackBar(
+                                        content: Text('Login successful')),
                                   );
                                 }
                               } catch (e) {
@@ -92,17 +99,24 @@ class LogIn extends ConsumerWidget {
                         backgroundColor: Colors.white,
                         foregroundColor: Colors.blueAccent,
                       ),
-                      child: signInState.isLoading ? const CircularProgressIndicator() : const Text('Login'),
+                      child: signInState.isLoading
+                          ? const CircularProgressIndicator()
+                          : const Text('Login'),
                     )),
                 OutlinedButton(
-                  child: const Text('Check the calendar function without logging in', style: TextStyle(color: Colors.grey)),
-
+                    child: const Text(
+                        'Check the calendar function without logging in',
+                        style: TextStyle(color: Colors.grey)),
                     onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(
-                          builder: (context) => const NoLoginCalendarPage()));
-                    }
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  const NoLoginCalendarPage()));
+                    }),
+                const SizedBox(
+                  height: 30,
                 ),
-                const SizedBox(height: 30,),
                 const MediumAdBanner(),
               ],
             ),
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,14 +1,6 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_workout_manager/core/logger.dart';
-
 import 'package:flutter_workout_manager/main.dart';
 
 void main() {
@@ -16,15 +8,13 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const ProviderScope(child: MyApp()));
 
-    // アプリ名が表示されていることを確認
+    // 0がないことを確認する
     expect(find.text('0'), findsNothing);
+    // アプリ名が表示されていることを確認
     expect(find.text('Home Fitness Manager'), findsOneWidget);
-
   });
 
-  tearDownAll((){
+  tearDownAll(() {
     logger.i('テスト終了');
   });
-
-
 }
