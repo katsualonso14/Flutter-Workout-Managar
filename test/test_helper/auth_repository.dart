f454bbe127435db7be09faafe7997d3ca17e7b38
katsuyatamai <xabi.k0124football@gmail.com>
2025-07-25 08:01:43 +0900

    add comment unused_import

--- test/test_helper/auth_repository.dart
+++ test/test_helper/auth_repository.dart
@@ -2,4 +2,5 @@ import 'package:flutter_workout_manager/domain/repositories/auth_repository.dart
 import 'package:mockito/annotations.dart';
 
 @GenerateNiceMocks([MockSpec<AuthRepository>()])
+// ignore: unused_import
 import 'auth_repository.mocks.dart';
